02afa12042f56c7a6c547a9cce72999011f53ef1
cabuelow <christinabuelow@gmail.com>
2022-09-20 16:31:07 +1000

    update script

--- scripts/scenarios-outcomes.R
+++ scripts/scenarios-outcomes.R
@@ -8,39 +8,47 @@ theme_set(theme_classic())
 source('scripts/models.R')
 source('scripts/helpers.R')
 
-# run scenario simulations
+#TODO: figure out why the 'none' scenario is messing things up
+
+# set up scenario simulations
 
 numsims <- 1000
+model <- modelA_driv
 
-sim_SLR <- system.sim_press(numsims, modelA_driv, perturb=c(SeaLevelRise=1))
-sim_SedSupp <- system.sim_press(numsims, modelA_driv, perturb=c(SedSupply=1))
-sim_SLR_SedSupp <- system.sim_press(numsims, modelA_driv, perturb=c(SeaLevelRise=1,
-                                                                 SedSupply=1))
-sim_SLF_SedSupp <- system.sim_press(numsims, modelA_driv, perturb=c(SeaLevelFall=1,
-                                                                 SedSupply=1))
+#actions = c('None', 'Restoration', 'Protection', 'PermeableDam')
+settings =  c('TidalAmp', 'TidalFreq', 'HydroEnergy', 'SedSupply')
+drivers= c('None', 'SeaLevelRise', 'SeaLevelFall', 'Cyclones', 'Dams', 'CoastalDev', 'BasementUp', 'Subsidence')
 
-#TODO: write a function to make all combinations of scenarios want to run, 
-# based on actions, geomorphology, and drivers
+all <- data.frame(#action = rep(actions, each = length(drivers)*length(settings)),
+                  setting = rep(settings, times = length(drivers)),
+                  driver = rep(drivers, times = length(settings)))
 
-actions <- c('Restoration', 'Protection', 'PermeableDam')
-settings <- c('TidalAmp', 'TidalFreq', 'HydroEnergy', 'SedSupply')
-drivers <- c('')
+# loop through scenarios with system.sim.press and store outcomes
 
-# then loop through scenarios with system.sim.press and store outcomes
+out <- list()
+
+for(i in 1:nrow(all)){
+  p <- rep(1, ncol(all))
+  names(p) <-  as.character(all[i,])
+  if(length(which(names(p) == 'None')) != 0){
+    p <- p[-which(names(p) == 'None')]
+  }
+  sim <- system.sim_press(numsims, model, perturb = p)
+  out[[i]] <- sim$stableoutcome
+}
 
 # wrangle outcomes
 
-outcomes <- data.frame(rbind(sim_SLR$stableoutcome, sim_SedSupp$stableoutcome, 
-                             sim_SLR_SedSupp$stableoutcome, sim_SLF_SedSupp$stableoutcome))
-
-outcomes$scnr <- rep(c("SLR", "Sediment supply", "Sediment supply & SLR", "Sediment supply & SLF"), 
-                     each = c(numsims*length(node.labels(modelA_driv))))
+outcomes <- do.call(rbind, out)
+outcomes$scnr <- rep(apply(all, 1, paste, collapse = ' & '), each = c(numsims*length(node.labels(model))))
+outcomes$setting <- rep(all$setting, each = c(numsims*length(node.labels(model))))
+outcomes$driver <- rep(all$driver, each = c(numsims*length(node.labels(model))))
 
 # calculate proportion of stable models that have positive, negative, or neutral outcome in landward/seaward mangrove response
 
 seaward <- outcomes %>% 
   filter(var == 'SeawardMang') %>% 
-  group_by(scnr) %>% 
+  group_by(scnr, setting, driver) %>% 
   summarise(Increase = sum(outcome>0)/n(),
             Neutral = sum(outcome==0)/n(),
             Decrease = sum(outcome<0)/n()) %>% 
@@ -49,32 +57,38 @@ seaward <- outcomes %>%
 
 landward <- outcomes %>% 
   filter(var == 'LandwardMang') %>% 
-  group_by(scnr) %>% 
+  group_by(scnr, setting, driver) %>% 
   summarise(Increase = sum(outcome>0)/n(),
             Neutral = sum(outcome==0)/n(),
             Decrease = sum(outcome<0)/n()) %>% 
   pivot_longer(Increase:Decrease ,names_to = 'outcome', values_to = 'prop') %>% 
   mutate(outcome = factor(outcome, levels = c('Increase', 'Neutral', 'Decrease')))
 
+landsea <- rbind(data.frame(seaward, mangrove = 'seaward'), data.frame(landward, mangrove = 'landward'))
+
 # Note, as potential TODO could boostrap resample here to get estimate of uncertainty around that probability
 
 a <- ggplot(seaward) +
-  geom_bar(aes(y = scnr, x = prop, fill = outcome),
+  geom_bar(aes(y = driver, x = prop, fill = outcome),
            position = 'stack', stat = 'identity') +
   xlab('Proportion of outcomes') +
   ylab('') +
   theme(legend.position = 'none') +
+  facet_wrap(~setting) +
   ggtitle('Seaward mangroves')
+a
 
 b <- ggplot(landward) +
-  geom_bar(aes(y = scnr, x = prop, fill = outcome),
+  geom_bar(aes(y = driver, x = prop, fill = outcome),
            position = 'stack', stat = 'identity') +
   xlab('Proportion of outcomes') +
   ylab('') +
+  facet_wrap(~setting) +
   ggtitle('Landward mangroves') +
   theme(legend.title = element_blank(),
         axis.text.y =  element_blank())
 
 a+b
 
-# now get parameter weights for increasing vs. decreasing
+ggsave('outputs/outcomes.png', width = 10, height = 5)
+
