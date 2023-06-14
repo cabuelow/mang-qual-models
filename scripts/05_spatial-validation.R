e053ab6e16f9d48d0f92fe605fed77a86342684a
cabuelow <christinabuelow@gmail.com>
2023-06-14 12:51:18 +1000

    update accuracy stat plotting

--- scripts/05_spatial-validation.R
+++ scripts/05_spatial-validation.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(caret)
 library(sf)
 library(scales)
+library(patchwork)
 source('scripts/helpers/models_v2.R')
 
 typ <- st_read('data/typologies/Mangrove_Typology_v3_Composite_valid_centroids.gpkg')
@@ -28,7 +29,9 @@ spatial_dat <- read.csv('outputs/master-dat.csv') %>%
          sea_net_gain_obs = ifelse(sea_net_gain == 1, 'Gain', 'No Gain'),
          sea_net_loss_obs = ifelse(sea_net_loss == 1, 'Loss', 'No Loss'),
          land_net_gain_obs = ifelse(land_net_gain == 1, 'Gain', 'No Gain'),
-         land_net_loss_obs = ifelse(land_net_loss == 1, 'Loss', 'No Loss'))      
+         land_net_loss_obs = ifelse(land_net_loss == 1, 'Loss', 'No Loss'),
+         land_net_change_obs = ifelse(land_net_gain == 1, 'Gain', 'Loss'),
+         sea_net_change_obs = ifelse(sea_net_gain == 1, 'Gain', 'Loss'))
 
 # which model outcomes to validate? Get outcomes for that model
 
@@ -40,35 +43,50 @@ dat <- read.csv(paste0('outputs/simulation-outcomes/outcomes_', chosen_model_nam
 
 # join outcomes to typologies and compare probability of loss/gain with historical gross loss/gain (1996-2020)
 
-threshold <- 75 # threshold for defining when a prediction is ambiguous or not
+threshold <- seq(60, 90, by = 5) # threshold for defining when a prediction is ambiguous or not
 
-land <- dat %>% 
-  pivot_wider(id_cols = -c(Prob_loss, Prob_gain_neutral), names_from = 'var', values_from = 'Prob_change') %>% 
-  mutate(Land_Gain = ifelse(LandwardMang > threshold, 'Gain', 'No Gain'),
-         Land_Ambig = ifelse(LandwardMang <= threshold & LandwardMang >= -threshold, 'Ambiguous', 'Not Ambiguous'),
-         Land_Loss = ifelse(LandwardMang < -threshold, 'Loss', 'No Loss')) %>% 
-  mutate(Land_Change = ifelse(Land_Gain == 'Gain', 'Gain', NA),
-         Land_Change = ifelse(Land_Ambig == 'Ambiguous', 'Ambiguous', Land_Change),
-         Land_Change = ifelse(Land_Loss == 'Loss', 'Loss', Land_Change)) %>% 
-  inner_join(select(spatial_dat, Type, sea_change_obs:land_net_loss_obs), by = 'Type') %>% 
-  select(Type, Land_Gain:Land_Change, land_change_obs, land_gain_obs, land_loss_obs, land_net_gain_obs, land_net_loss_obs)
+# land
+tmp <- list()
+for(i in seq_along(threshold)){
+  thresh <- threshold[i]
+  tmp[[i]] <- dat %>% 
+    pivot_wider(id_cols = -c(Prob_loss, Prob_gain_neutral), names_from = 'var', values_from = 'Prob_change') %>% 
+    mutate(Land_Gain = ifelse(LandwardMang > thresh, 'Gain', 'No Gain'),
+           Land_Ambig = ifelse(LandwardMang <= thresh & LandwardMang >= -thresh, 'Ambiguous', 'Not Ambiguous'),
+           Land_Loss = ifelse(LandwardMang < -thresh, 'Loss', 'No Loss')) %>% 
+    mutate(Land_Change = ifelse(Land_Gain == 'Gain', 'Gain', NA),
+           Land_Change = ifelse(Land_Ambig == 'Ambiguous', 'Ambiguous', Land_Change),
+           Land_Change = ifelse(Land_Loss == 'Loss', 'Loss', Land_Change)) %>% 
+    inner_join(select(spatial_dat, Type, sea_change_obs:sea_net_change_obs), by = 'Type') %>% 
+    select(Type, Land_Gain:Land_Change, land_change_obs, land_gain_obs, land_loss_obs, land_net_gain_obs, land_net_loss_obs, land_net_change_obs) %>% 
+    mutate(ambig_threshold = thresh)
+}
+land <- do.call(rbind, tmp)
 write.csv(land, 'outputs/validation/land-validation-results.csv', row.names = F)
 
-sea <- dat %>% 
-  pivot_wider(id_cols = -c(Prob_loss, Prob_gain_neutral), names_from = 'var', values_from = 'Prob_change') %>% 
-  mutate(Sea_Gain = ifelse(SeawardMang > threshold, 'Gain', 'No Gain'),
-         Sea_Ambig = ifelse(SeawardMang <= threshold & SeawardMang >= -threshold, 'Ambiguous', 'Not Ambiguous'),
-         Sea_Loss = ifelse(SeawardMang < -threshold, 'Loss', 'No Loss')) %>% 
-  mutate(Sea_Change = ifelse(Sea_Gain == 'Gain', 'Gain', NA),
-         Sea_Change = ifelse(Sea_Ambig == 'Ambiguous', 'Ambiguous', Sea_Change),
-         Sea_Change = ifelse(Sea_Loss == 'Loss', 'Loss', Sea_Change)) %>% 
-  inner_join(select(spatial_dat, Type, sea_change_obs:sea_net_loss_obs), by = 'Type') %>% 
-  select(Type, Sea_Gain:Sea_Change, sea_change_obs, sea_gain_obs, sea_loss_obs, sea_net_gain_obs, sea_net_loss_obs)
+# sea
+tmp <- list()
+for(i in seq_along(threshold)){
+  thresh <- threshold[i]
+  tmp[[i]] <- dat %>% 
+    pivot_wider(id_cols = -c(Prob_loss, Prob_gain_neutral), names_from = 'var', values_from = 'Prob_change') %>% 
+    mutate(Sea_Gain = ifelse(SeawardMang > thresh, 'Gain', 'No Gain'),
+           Sea_Ambig = ifelse(SeawardMang <= thresh & SeawardMang >= -thresh, 'Ambiguous', 'Not Ambiguous'),
+           Sea_Loss = ifelse(SeawardMang < -thresh, 'Loss', 'No Loss')) %>% 
+    mutate(Sea_Change = ifelse(Sea_Gain == 'Gain', 'Gain', NA),
+           Sea_Change = ifelse(Sea_Ambig == 'Ambiguous', 'Ambiguous', Sea_Change),
+           Sea_Change = ifelse(Sea_Loss == 'Loss', 'Loss', Sea_Change)) %>% 
+    inner_join(select(spatial_dat, Type, sea_change_obs:sea_net_change_obs), by = 'Type') %>% 
+    select(Type, Sea_Gain:Sea_Change, sea_change_obs, sea_gain_obs, sea_loss_obs, sea_net_gain_obs, sea_net_loss_obs, sea_net_change_obs) %>% 
+    mutate(ambig_threshold = thresh)
+}
+sea <- do.call(rbind, tmp)
 write.csv(sea, 'outputs/validation/sea-validation-results.csv', row.names = F)
 
 # calculate overall prediction/classification accuracy
 # and commission (users accuracy) and omission (producers accuracy) for each class
 
+# function
 calc_accuracy <- function(x, x2){ # x is vector of predictions, x2 is reference vector
 cont.table <- confusionMatrix(factor(x), factor(x2))$table # contingency table
 commission <- diag(cont.table)/rowSums(cont.table)*100
@@ -80,25 +98,159 @@ names(accuracy_list) <- c('accuracy.results', 'contingency.table')
 return(accuracy_list)
 }
 
-# individual classes, net change
+# remove ambiguous responses
 land_validate <- filter(land, Land_Ambig != 'Ambiguous') # get rid of ambiguous responses, can't validate
 sea_validate <- filter(sea, Sea_Ambig != 'Ambiguous') # get rid of ambiguous responses, can't validate
 
-calc_accuracy(land_validate$Land_Gain, land_validate$land_net_gain_obs)
-calc_accuracy(land_validate$Land_Loss, land_validate$land_net_loss_obs)
+# net change validation
+tmp <- list()
+for(i in seq_along(threshold)){
+  thresh <- threshold[i]
+  landval <- land_validate %>% filter(ambig_threshold == thresh)
+  seaval <- sea_validate %>% filter(ambig_threshold == thresh)
+  results <- data.frame(validation = 'net',
+                        mangrove = 'Landward',
+                        ambig_threshold = thresh, 
+                        calc_accuracy(landval$Land_Change, landval$land_net_change_obs)$accuracy.results)
+  results2 <- data.frame(validation = 'net',
+                        mangrove = 'Seaward',
+                        ambig_threshold = thresh, 
+                        calc_accuracy(seaval$Sea_Change, seaval$sea_net_change_obs)$accuracy.results)
+  tmp[[i]] <- rbind(results, results2)
+}
+net_val <- do.call(rbind, tmp)
 
-calc_accuracy(sea_validate$Sea_Gain, sea_validate$sea_net_gain_obs)
-calc_accuracy(sea_validate$Sea_Loss, sea_validate$sea_net_loss_obs)
+# gross gains/losses validation
+tmp <- list()
+for(i in seq_along(threshold)){
+  thresh <- threshold[i]
+  landval <- land_validate %>% filter(ambig_threshold == thresh)
+  seaval <- sea_validate %>% filter(ambig_threshold == thresh)
+  results <- data.frame(validation = 'gross',
+                        mangrove = 'Landward',
+                        ambig_threshold = thresh, 
+                        calc_accuracy(landval$Land_Gain, landval$land_gain_obs)$accuracy.results)
+  results2 <- data.frame(validation = 'gross',
+                        mangrove = 'Landward',
+                        ambig_threshold = thresh, 
+                        calc_accuracy(landval$Land_Loss, landval$land_loss_obs)$accuracy.results)
+  results3 <- data.frame(validation = 'gross',
+                         mangrove = 'Seaward',
+                         ambig_threshold = thresh, 
+                         calc_accuracy(seaval$Sea_Gain, seaval$sea_gain_obs)$accuracy.results)
+  results4 <- data.frame(validation = 'gross',
+                         mangrove = 'Seaward',
+                         ambig_threshold = thresh, 
+                         calc_accuracy(seaval$Sea_Loss, seaval$sea_loss_obs)$accuracy.results)
+  tmp[[i]] <- rbind(results, results2, results3, results4)
+}
+gross_val <- do.call(rbind, tmp)
+accuracy <- rbind(net_val, gross_val) %>% mutate(mangrove = factor(mangrove, levels = c('Seaward', 'Landward')))
+write.csv(accuracy, 'outputs/validation/accuracy-threshold-vary.csv', row.names = F)
 
-# individual classes, gross loss/gain
-land_validate <- filter(land, Land_Ambig != 'Ambiguous') # get rid of ambiguous responses, can't validate
-sea_validate <- filter(sea, Sea_Ambig != 'Ambiguous') # get rid of ambiguous responses, can't validate
+# plot accuracy results
 
-calc_accuracy(land_validate$Land_Gain, land_validate$land_gain_obs)
-calc_accuracy(land_validate$Land_Loss, land_validate$land_loss_obs)
+# net
+a <- accuracy %>% 
+  filter(validation == 'net') %>% 
+  pivot_longer(cols = c(overall_accuracy:commission_accuracy), names_to = 'accuracy_cat', values_to = 'accuracy') %>% 
+  mutate(remove = ifelse(accuracy_cat == 'overall_accuracy' & class == 'Loss', 1, 0)) %>% 
+  filter(remove == 0) %>% 
+  mutate(cat = ifelse(accuracy_cat != 'overall_accuracy', paste0(class, '_', accuracy_cat), accuracy_cat)) %>% 
+  mutate(cat = factor(cat, levels = c('overall_accuracy', 'Gain_commission_accuracy', 'Loss_commission_accuracy',
+                                      'Gain_omission_accuracy', 'Loss_omission_accuracy'))) %>% 
+  ggplot() +
+  geom_line(aes(x = ambig_threshold, y = accuracy, col = cat), linewidth = 1, alpha = 0.5) +
+  facet_wrap(~mangrove) +
+  ylim(c(0,100)) +
+  ylab('Accuracy') +
+  xlab('Ambiguity probability threshold (%)') +
+  scale_color_manual(values = c("black",'darkslategray3', 'darkolivegreen3',
+                                'goldenrod3', 'lightcoral'),
+                     labels = c('Overall accuracy', 'Gain commission accuracy',
+                                'Loss commission accuracy', 'Gain omission accuracy',
+                                'Loss omission accuracy')) +
+  geom_vline(xintercept = 75, linetype = 'dashed', alpha = 0.4) +
+  ggtitle('A) Net change') +
+  theme_classic() +
+  theme(legend.title = element_blank(),
+        legend.position = 'bottom',
+        plot.title = element_text(size = 11)) +
+  guides(color=guide_legend(ncol=1))
+a
 
-calc_accuracy(sea_validate$Sea_Gain, sea_validate$sea_gain_obs)
-calc_accuracy(sea_validate$Sea_Loss, sea_validate$sea_loss_obs)
+# gross losses
+b <- accuracy %>% 
+  filter(validation == 'gross') %>% 
+  mutate(type = ifelse(class == 'No Gain', 'Gain', NA),
+         type = ifelse(class == 'Gain', 'Gain', type),
+         type = ifelse(class == 'Loss', 'Loss', type),
+         type = ifelse(class == 'No Loss', 'Loss', type)) %>% 
+  filter(type == 'Loss') %>% 
+  pivot_longer(cols = c(overall_accuracy:commission_accuracy), names_to = 'accuracy_cat', values_to = 'accuracy') %>% 
+  mutate(remove = ifelse(accuracy_cat == 'overall_accuracy' & class == 'No Loss', 1, 0)) %>% 
+  mutate(remove = ifelse(accuracy_cat == 'overall_accuracy' & class == 'No Gain', 1, remove)) %>% 
+  filter(remove == 0) %>% 
+  mutate(cat = ifelse(accuracy_cat != 'overall_accuracy', paste0(class, '_', accuracy_cat), accuracy_cat)) %>%
+  mutate(cat = factor(cat, levels = c('overall_accuracy', 'Loss_commission_accuracy', 'No Loss_commission_accuracy',
+                                      'Loss_omission_accuracy', 'No Loss_omission_accuracy'))) %>% 
+  ggplot() +
+  geom_line(aes(x = ambig_threshold, y = accuracy, col = cat), linewidth = 1, alpha = 0.5) +
+  facet_wrap(~mangrove) +
+  ylim(c(0,100)) +
+  ylab('') +
+  xlab('Ambiguity probability threshold (%)') +
+  scale_color_manual(values = c("black",'darkslategray3', 'darkolivegreen3',
+                                'goldenrod3', 'lightcoral'),
+                     labels = c('Overall accuracy', 'Loss commission accuracy',
+                                'No Loss commission accuracy', 'Loss omission accuracy',
+                                'No Loss omission accuracy')) +
+  geom_vline(xintercept = 75, linetype = 'dashed', alpha = 0.4) +
+  ggtitle('B) Gross losses') +
+  theme_classic() +
+  theme(legend.title = element_blank(),
+        legend.position = 'bottom',
+        plot.title = element_text(size = 11)) +
+  guides(color=guide_legend(ncol=1))
+b
+
+# gross gains
+c <- accuracy %>% 
+  filter(validation == 'gross') %>% 
+  mutate(type = ifelse(class == 'No Gain', 'Gain', NA),
+         type = ifelse(class == 'Gain', 'Gain', type),
+         type = ifelse(class == 'Loss', 'Loss', type),
+         type = ifelse(class == 'No Loss', 'Loss', type)) %>% 
+  filter(type == 'Gain') %>% 
+  pivot_longer(cols = c(overall_accuracy:commission_accuracy), names_to = 'accuracy_cat', values_to = 'accuracy') %>% 
+  mutate(remove = ifelse(accuracy_cat == 'overall_accuracy' & class == 'No Loss', 1, 0)) %>% 
+  mutate(remove = ifelse(accuracy_cat == 'overall_accuracy' & class == 'No Gain', 1, remove)) %>% 
+  filter(remove == 0) %>% 
+  mutate(cat = ifelse(accuracy_cat != 'overall_accuracy', paste0(class, '_', accuracy_cat), accuracy_cat)) %>% 
+  mutate(cat = factor(cat, levels = c('overall_accuracy', 'Gain_commission_accuracy', 'No Gain_commission_accuracy',
+                                      'Gain_omission_accuracy', 'No Gain_omission_accuracy'))) %>% 
+  ggplot() +
+  geom_line(aes(x = ambig_threshold, y = accuracy, col = cat), linewidth = 1, alpha = 0.5) +
+  facet_wrap(~mangrove) +
+  ylim(c(0,100)) +
+  ylab('') +
+  xlab('Ambiguity probability threshold (%)') +
+  scale_color_manual(values = c("black",'darkslategray3', 'darkolivegreen3',
+                                'goldenrod3', 'lightcoral'),
+                     labels = c('Overall accuracy', 'Gain commission accuracy',
+                                'No Gain commission accuracy', 'Gain omission accuracy',
+                                'No Gain omission accuracy')) +
+  geom_vline(xintercept = 75, linetype = 'dashed', alpha = 0.4) +
+  ggtitle('C) Gross gains') +
+  theme_classic() +
+  theme(legend.title = element_blank(),
+        legend.position = 'bottom',
+        plot.title = element_text(size = 11)) +
+  guides(color=guide_legend(ncol=1))
+c
+
+a+b+c
+ggsave('outputs/validation/accuracy-plot.png', width = 11, height = 4)
 
 # summarise characteristics of typologies with gains, losses, or ambiguity
 
@@ -129,6 +281,7 @@ ggplot(land_sum) +
   scale_fill_distiller(palette = 'YlOrRd', direction = 1,  name = '') +
   xlab('') +
   ylab('') +
+  ggtitle('B) Landward') +
   theme_classic()
 
 #sea
