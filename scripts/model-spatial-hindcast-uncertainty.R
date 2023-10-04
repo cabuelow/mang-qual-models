4860bb1f79133527a4b920fdf2aa4224ec434401
cabuelow <christinabuelow@gmail.com>
2023-10-04 16:39:10 +1000

    add script for using resampling to quantify accuracy uncertainty and propagate to forecasts

---
+++ scripts/model-spatial-hindcast-uncertainty.R
@@ -0,0 +1,8 @@
+# quantify uncertainty in model hindcast accuracy
+# bootstrap resample data (ie random sampling with replacement) into 5 folds 40 times (which is 5*40 = 200 test-training pairs, recommended as stable by Lyons et al. 2018)
+# to obtain sampling distribution of each accuracy estimate (overall, producers, users)
+# can use median and 0.025 and 0.975 percentiles to estimate 95% confidence intervals around accuracy estimate
+# propagate uncertainty to estimates of number of units of loss, gain/neutrality, or ambiguity
+# by taking the 95th percentiles of the commission and omission errors for each class
+# num_gain_units(upper) = num_gain_units + (num_gain_units * omission_error(95 percentile)) # omission error is the rate of false negatives, so predictions of a class we are missing, so is the upper
+# num_gain_units(lower) = num_gain_units - (num_gain_units * comission_error(95 percentile)) # commission error is the rate of false positives, so predictions of a class we shouldn't have, so is the lower
